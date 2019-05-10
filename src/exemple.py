6c9cb631a113ef7b468507c81034a0ea4c6f80a0
Nicolay Dimitrov Vasilev <nicolaypsp@gmail.com>
2019-05-10 13:15:05 +0200

    PyWren invoking RabbitAMPQ

--- src/exemple.py
+++ src/exemple.py
@@ -4,5 +4,19 @@ Created on 8 may. 2019
 @author: aaroni34
 '''
 
-import pywren_ibm_cloud
+import random
+import pywren_ibm_cloud as pywren
 
+
+def my_map_function(ide):
+    num = random.randint(0,1000)
+    return num
+
+pw = pywren.ibm_cf_executor(rabbitmq_monitor=True)
+
+
+pw = pywren.ibm_cf_executor(rabbitmq_monitor=True)
+pw.map(my_map_function, range(2))
+pw.monitor()
+pw.create_timeline_plots('/Users/nicolaydv/Desktop', 'rabbitmq')
+pw.clean()
