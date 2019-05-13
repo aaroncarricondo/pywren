61e5a844e6a67236bca5f8ff580949986a5d3de5
Nicolay Dimitrov Vasilev <nicolaypsp@gmail.com>
2019-05-13 11:08:56 +0200

    Added callback function and pywren execution

--- src/exemple.py
+++ src/exemple.py
@@ -4,24 +4,72 @@ Created on 8 may. 2019
 @author: aaroni34
 '''
 
-import random, yaml, pika
+import random, yaml, pika, sys, os
 import pywren_ibm_cloud as pywren
+
+iterdata = range(int(sys.argv[1]))
+
+
+
+        
 
 
 def my_map_function(ide):
-    num = random.randint(0,1000)
-    return num
+    
+    array=[]
+    messages=0
+    
+    def callback(channel, method, header, body): 
+        # We've received numDiv messages, stop consuming
+        global messages
+        global array 
+        channel.basic_ack(delivery_tag = method.delivery_tag)
+        array.append(body)
+        messages += 1
+        if messages >= len(iterdata):
+            channel.stop_consuming()
+    
+    if (ide == 0):
+        im_boss = True
+    else:
+        im_boss = False
+        
+    if (im_boss):
+        waiting = []
+    
+    
+    params = pika.URLParameters(str(os.environ.get('rabbit')))
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel() # start a channel
+    #channel.exchange_declare(exchange='logs', exchange_type='fanout')
+    
+   
+    channel.queue_declare(str(ide))
+    
+    #Define function when consuming
+    channel.basic_consume(callback, queue=str(ide))
+    
+    #Bind queue to exchange
+    channel.queue_bind(exchange='logs', queue=str(ide))
+    
+    num = random.randint(0,50)
+    channel.basic_publish(exchange='logs', routing_key='', body=str(num))
+    
+    channel.start_consuming()
+    
+    
+    return iterdata
 
 
 
-#Load COS, Functions and RabbitAMQP information
+
+
+
+#Load RabbitAMQP information
 with open('ibm_cloud_config', 'r') as config_file:
-    try:
-        
-        res = yaml.safe_load(config_file)
-        
-    except yaml.YAMLError as exc:
-        
+    try:        
+        res = yaml.safe_load(config_file)        
+    except yaml.YAMLError as exc:        
         print(exc)
         
 #Get URL
@@ -31,18 +79,22 @@ rabbit = res['ibm_rabbit']
 params = pika.URLParameters(str(rabbit))
 connection = pika.BlockingConnection(params)
 channel = connection.channel() # start a channel
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+
 
 #----------------------
 #----- QUEUE ------
-channel.queue_delete('queue')
-channel.queue_declare(queue='queue')
+#channel.queue_delete('queue')
+#channel.queue_declare(queue='queue')
+
+
+pw = pywren.ibm_cf_executor();
+extra_env={'rabbit' : rabbit}
+pw.map(my_map_function, iterdata, extra_env = extra_env)
+result = pw.get_result()
+print(result)
 
 
 
-pw = pywren.ibm_cf_executor(rabbitmq_monitor=True)
-
-
-pw = pywren.ibm_cf_executor(rabbitmq_monitor=True)
-pw.map(my_map_function, range(2))
-pw.
-pw.clean()
+print("TODO OK")
