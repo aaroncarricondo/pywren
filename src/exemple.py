f80c3588c82249a1917afd085d795154758681f6
Aaron Carricondo <aaron_carricondo@hotmail.com>
2019-05-15 10:09:17 +0200

    Began master function with its normal queue and callback function modified

--- src/exemple.py
+++ src/exemple.py
@@ -4,21 +4,20 @@ Created on 8 may. 2019
 @author: aaroni34
 '''
 
-import random, yaml, pika, sys, os, time
+import random, yaml, pika, sys, os
 import pywren_ibm_cloud as pywren
 
 iterdata = range(int(sys.argv[1]))
 
-
-array = []
-counter = 0
-
-def my_map_function(ide):
+def my_master_function():
     
-    def callback(channel, method, header, body):
+    array = []
+    counter = 0
+    
+    def callback_master(channel, method, header, body):
         
-        global array
-        global counter
+        nonlocal counter 
+        nonlocal array 
         # We've received numDiv messages, stop consuming
         channel.basic_ack(delivery_tag = method.delivery_tag)
         
@@ -29,24 +28,66 @@ def my_map_function(ide):
             channel.stop_consuming()
     
     
-    if (ide == 0):
-        im_boss = True
-    else:
-        im_boss = False
-        
-    if (im_boss):
-        waiting = []
-    
-    
     params = pika.URLParameters(str(os.environ.get('rabbit')))
     connection = pika.BlockingConnection(params)
     channel = connection.channel() # start a channel
     
+    channel.queue_declare(queue='master', auto_delete=True)
+    
+    channel.basic_consume(callback_master, queue='master')
+    channel.start_consuming()
+    
+    num = random.randint(0,len(iterdata)) - 1
+    
+    write = array[num]
+    
+    channel.basic_publish(exchange='logs', routing_key='', body='W ' + write)
+    
+    
+    
+    connection.close()
+    
+    
+
+
+def my_map_function(ide):
+    
+    array = []
+    counter = 0
+    
+    def callback(channel, method, header, body):
+        
+        nonlocal counter 
+        nonlocal array 
+        # We've received numDiv messages, stop consuming
+        channel.basic_ack(delivery_tag = method.delivery_tag)
+        
+        body = body.decode('utf-8')
+        header, body = body.split(' ')
+        
+        if (header == 'W'):
+            
+            if (body == str(ide)):
+                channel.basic_publish(exchange='logs', routing_key='', body=str(num))
+                
+        else:
+            #Append random number to array and increment counter
+            array.append(body)
+            counter = counter + 1
+            if (counter >= len(iterdata)): 
+                channel.stop_consuming()
+    
+    params = pika.URLParameters(str(os.environ.get('rabbit')))
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel() # start a channel
+    #Generate random
     num = random.randint(0,200)
-    channel.basic_publish(exchange='logs', routing_key='', body=str(num))
+    
+    #Publish want to write (ide) in the topic queue of the master
+    channel.basic_publish(exchange='', routing_key='master', body= str(ide))
+    
     #Associate
     channel.basic_consume(callback, queue=str(ide))
-    
     #Start consuming and processing values
     channel.start_consuming()
     
@@ -54,7 +95,6 @@ def my_map_function(ide):
     connection.close()
     
     return array
-
 
 
 #Load RabbitAMQP information
@@ -73,8 +113,6 @@ channel = connection.channel() # start a channel
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
 
-#----------------------
-#----- QUEUES ------
 for i in iterdata:
     channel.queue_declare(queue=str(i), auto_delete=True)
     channel.queue_bind(exchange='logs', queue=str(i))
